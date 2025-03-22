478d7ee5d1437c4d546cb73e9a7269c7b697c513
Robb-Fr <git@robb-i.net>
2025-03-22 19:33:20 +0100

    adds character selection logic

---
+++ character_select.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var characters = [
+	["Gégé", "res://art/atomic_petanque/gégé_moustache_de_feu.png"],
+	["Oliv", "res://art/atomic_petanque/oliv_chaussettes_deparaillees.png"],
+	["Patoche", "res://art/atomic_petanque/patoche_au_bob.png"],
+	["Nico", "res://art/atomic_petanque/nico_la_gaypride.png"]
+	]
+
+signal selected_character(index: int)
+
+@export var current_char = 0
+
+func _ready():
+	$HBoxContainer/CharacterName.text = characters[current_char][0]
+	var texture: Texture2D = load(characters[current_char][1])
+	$HBoxContainer/CharacterIcon.set_texture(texture)
+
+func _on_change_character_pressed():
+	var next_char_index = (current_char + 1) % len(characters)
+	var next_char = characters[(current_char + 1) % len(characters)]
+	$HBoxContainer/CharacterName.text = next_char[0]
+	var next_texture: Texture2D = load(next_char[1])
+	$HBoxContainer/CharacterIcon.set_texture(next_texture)
+	current_char = next_char_index
+
+func _on_select_character_pressed():
+	$HBoxContainer/SelectCharacter.disabled = true
+	$HBoxContainer/ChangeCharacter.disabled = true
+	selected_character.emit(current_char)
--- hud.gd
+++ hud.gd
@@ -2,6 +2,14 @@ extends CanvasLayer
 
 # Notifies `Main` node that the button has been pressed
 signal start_game
+signal all_player_selected(player: Array[int])
+signal new_player_selected(player_index: int, character_index: int)
+
+var selected_characters = [-1, -1, -1, -1]
+
+func _ready():
+	$PlayerSelects.hide()
+	$ScoreLabel.hide()
 
 func show_message(text):
 	$Message.text = text
@@ -21,10 +29,34 @@ func show_game_over():
 	
 func update_score(score):
 	$ScoreLabel.text = str(score)
-	
+
 func _on_start_button_pressed():
 	$StartButton.hide()
-	start_game.emit()
+	$Message.hide()
+	$PlayerSelects.show()
 
 func _on_message_timer_timeout():
 	$Message.hide()
+
+func _on_all_player_selected():
+	$CharacterSelectionList.hide()
+	all_player_selected.emit()
+
+func _on_select_player_1_selected_character(index):
+	new_player_selected.emit(0, index)
+
+func _on_select_player_2_selected_character(index):
+	new_player_selected.emit(1, index)
+
+func _on_select_player_3_selected_character(index):
+	new_player_selected.emit(2, index)
+
+func _on_select_player_4_selected_character(index):
+	new_player_selected.emit(3, index)
+
+func _on_new_player_selected(player_index, character_index):
+	selected_characters[player_index] = character_index
+	if not selected_characters.has(-1):
+		all_player_selected.emit(selected_characters)
+		print_debug("Selected characters array: " + str(selected_characters))
+		$PlayerSelects.queue_free()
