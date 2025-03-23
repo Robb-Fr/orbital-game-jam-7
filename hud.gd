cb58d425aa9d5a0705361f82bc4a2591acc32cde
MaelysBillon <maelys.billon@epfl.ch>
2025-03-23 07:07:54 +0100

    Hallelujah

--- hud.gd
+++ hud.gd
@@ -6,7 +6,13 @@ var nb_players = 2
 # Notifies `Main` node that the button has been pressed
 signal all_players_selected(players: Array[Array])
 
+signal start_minigame
+
+signal all_players_selected_bis(characters: Array)
+
 var selected_characters = [-1, -1, -1, -1]
+
+var mae_char= [-1,-1,-1,-1]
 
 func _ready():
 	$HeaderMessage.hide()
@@ -14,6 +20,7 @@ func _ready():
 	$ScoreLabel.hide()
 	$NbPlayersBox.hide()
 	$EnterNbPlayers.hide()
+	$Musique_festive.play()
 
 func on_selected_character(char_name: String, texture: Texture2D, player_index: int):
 	selected_characters[player_index] = [char_name, texture]
@@ -47,10 +54,22 @@ func _on_message_timer_timeout():
 	$Message.hide()
 
 func _on_all_players_selected(players):
+	var i=0
+	for item in players:
+		if item is int:
+			pass
+		else:
+			mae_char[i]=item[0]
+		i=i+1
+	print_debug(mae_char)
+	all_players_selected_bis.emit(mae_char)
 	$HeaderMessage.hide()
 	$PlayerSelects.queue_free()
 	$Background.hide()
+	$Musique_festive.stop()
 	show_message("Press fast to DRINK")
+	start_minigame.emit()
+	print_debug("MAIS BORDEL")
 
 func _on_enter_nb_players_pressed():
 	$NbPlayersBox.hide()
